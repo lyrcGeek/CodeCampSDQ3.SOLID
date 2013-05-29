f2f57e283601bbc051e818a2e2434fdf2d768a39
Luis Y. Ramirez <lyrc@outlook.com>
2013-05-29 15:26:47 -0400

    Adding Solution to Source Control

---
+++ SimpleResponsabilityPrinciple/Bad/LoginManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SimpleResponsabilityPrinciple.Bad
+{
+    public class LoginManager
+    {
+        public bool AutenticateUser(User user)
+        {
+            var result = false;
+
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = @"SELECT UserName FROM Users WHERE UserName = '" + user.UserName + "'" + 
+                                    "AND Password = '" + user.UserName + "'";
+
+            try
+            {
+                connection.Open();
+                string name = command.ExecuteScalar().ToString();
+                if (!string.IsNullOrEmpty(name))
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                StringWriter logger = new StringWriter();
+                logger.WriteLine(ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
