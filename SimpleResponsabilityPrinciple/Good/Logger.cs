a3330b0555c4251480cd4d7ced0887d432c70502
lyrcprojects <lyrc@outlook.com>
2013-06-20 15:56:28 -0400

    SRP Example Ready

---
+++ SimpleResponsabilityPrinciple/Good/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SingleResponsabilityPrinciple.Good
+{
+    public class Logger
+    {
+        private StringWriter logger;
+
+        public Logger()
+        {
+            logger = new StringWriter();
+        }
+
+        public void WriteToLog(string value)
+        {
+            logger.WriteLine(value);
+        }
+
+        public void SaveLog()
+        {
+            //Do stuff for Save log to File
+        }
+    }
+}
--- SimpleResponsabilityPrinciple/Good/LoginManager.cs
+++ SimpleResponsabilityPrinciple/Good/LoginManager.cs
@@ -1,11 +1,81 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
 namespace SingleResponsabilityPrinciple.Good
 {
-    class LoginManager
+    public class LoginManager
     {
+        private Logger logger;
+
+        public LoginManager()
+        {
+            logger = new Logger();
+        }
+
+        public bool AuthenticateUser(User user)
+        {
+            var command = CreateAuthenticationCommandFromUser(user);
+
+            try
+            {
+                ConnectionManager.OpenConnection();
+
+                string userName = command.ExecuteScalar().ToString();
+                
+                return IsValidUser(userName);
+            }
+            catch (Exception ex)
+            {
+                logger.WriteToLog(ex.Message);
+                logger.SaveLog();
+                return false;
+            }
+            finally
+            {
+                ConnectionManager.CloseConnection();
+            }
+        }
+
+        private bool IsValidUser(string userName)
+        {
+            bool result = false;
+
+            if (!string.IsNullOrEmpty(userName))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        private SqlCommand CreateAuthenticationCommandFromUser(User user)
+        {
+            var command = GetCommand("uspAuthenticateUser", CommandType.StoredProcedure);
+            
+            var parameters = new SqlParameter[] 
+            { 
+                new SqlParameter("@UserName", SqlDbType.VarChar, 10){Value = user.UserName},
+                new SqlParameter("@Password", SqlDbType.VarChar, 20){Value = user.Password},
+            };
+
+            command.Parameters.AddRange(parameters);
+
+            return command;
+        }
+
+        private SqlCommand GetCommand(string commandText, CommandType commandType)
+        {
+            var command = ConnectionManager.GetConnection().CreateCommand();
+            command.Connection = ConnectionManager.GetConnection();
+            command.CommandText = commandText;
+            command.CommandType = commandType;
+
+            return command;
+        }
     }
 }
