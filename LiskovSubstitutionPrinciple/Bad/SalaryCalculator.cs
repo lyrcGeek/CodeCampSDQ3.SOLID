bc6611a9a129e5bf7512dc7cd8d31048a6a7a438
lyrcprojects <lyrc@outlook.com>
2013-06-27 01:02:33 -0400

    Name changes on Projects, then, need to upload all files

---
+++ LiskovSubstitutionPrinciple/Bad/SalaryCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiskovSubstitutionPrinciple.Bad
+{
+    public class SalaryCalculator
+    {
+        private double tax = 35;
+
+        public double CalculateNetSalary(Person person)
+        {
+            if (person.GetType() == typeof(Vendor))
+            {
+                var vendor = (Vendor)person;
+                return (vendor.Salary - tax) + vendor.Comission;
+            }
+            else if (person.GetType() == typeof(Secretary))
+            {
+                var secretary = (Secretary)person;
+                return (person.Salary - tax) + secretary.Incentive;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ LiskovSubstitutionPrinciple/Bad/Secretary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiskovSubstitutionPrinciple.Bad
+{
+    public class Secretary : Person
+    {
+        public double Incentive { get; set; }
+    }
+}
